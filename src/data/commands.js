55fe3cf354c7eff067a9e5ea31275d8cc614bb97
shasherazi <hassanrandomz@gmail.com>
2023-02-17 19:02:41 +0500

    Added some more commands

--- src/data/commands.js
+++ src/data/commands.js
@@ -1,6 +1,14 @@
 export default [
   {
     name: 'help',
-    response: 'site is still under construction',
+    response: 'welcome to my portfolio. there isn\'t much going around here rn <br/> as of now, these are the only commands that you can use: <br/> <br/> <strong>help</strong> - shows this message <br/> <strong>about</strong> - shows a little bit about me <br/> <strong>contact</strong> - shows my contact info',
+  },
+  {
+    name: 'about',
+    response: 'i\'m a 19-year-old (age might not be up to date, i was born in 2003, do your math) Pakistani who loves to code. currently, i\'m an undergraduate student at <a href="https://www.uopeople.edu/" target="_blank">University of the People</a>. these days i\'m enrolled at a bootcamp called <a href="https://www.microverse.org/" target="_blank">Microverse</a> where i work on remote projects with developers from all around the world',
+  },
+  {
+    name: 'contact',
+    response: 'here is my encrtyped email address: cvnnvimviyjhu@bhvdg.xjh <br /> start your mail with <strong>veni, vidi, vici</strong> if you successfully decrypt my email address and i\'ll get back to you as soon as possible',
   },
 ];
